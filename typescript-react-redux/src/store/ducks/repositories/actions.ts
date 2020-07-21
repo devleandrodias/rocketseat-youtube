e4b751706d9363f8bd447ad13b45fb47b9b425f6
devleandrodias <leandrodbdias@gmail.com>
2020-07-21 08:44:09 -0300

    limpando repositórioantigo

--- typescript-react-redux/src/store/ducks/repositories/actions.ts
+++
@@ -1,6 +0,0 @@
-import { action } from 'typesafe-actions';
-import { RepositoryTypes, Repository } from './types';
-
-export const loadRequest = () => action(RepositoryTypes.LOAD_REQUEST);
-export const loadSuccess = (data: Repository[]) => action(RepositoryTypes.LOAD_SUCCESS, data);
-export const loadFailure = () => action(RepositoryTypes.LOAD_FAILURE);
